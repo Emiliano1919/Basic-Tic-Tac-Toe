c2b749bc6126119f05a8c740f47e857696958249
Emiliano1919 <emilianojg38@gmail.com>
2023-07-24 07:56:15 -0600

    Make the restart button functional and modal

--- javascript.js
+++ javascript.js
@@ -2,6 +2,8 @@ let btnStart = document.querySelector("#start");
 let modal = document.querySelector("#modal");
 let closing = document.querySelector(".close-modal");
 let submit = document.querySelector("#submit");
+let main = document.querySelector('.main');
+let btnReStart= document.querySelector('#restart');
 
 let box1 = document.querySelector(".box1");
 let box2 = document.querySelector(".box2");
@@ -18,10 +20,38 @@ btnStart.addEventListener('click', () => {
     btnStart.remove();
     modal.classList.remove('notOpen');
     modal.classList.add('opened');
+    btnReStart.classList.remove('notOpen');
+    btnReStart.textContent='ReStart';
+    btnReStart.classList.toggle('restart');
+    main.appendChild(btnReStart);
 });
+
+
+
 
 box1.addEventListener('click', () => {
     box1.classList.add('player1');
+});
+
+btnReStart.addEventListener('click', () => {
+    box1.classList.remove('player1');
+    box2.classList.remove('player1');
+    box3.classList.remove('player1');
+    box4.classList.remove('player1');
+    box5.classList.remove('player1');
+    box6.classList.remove('player1');
+    box7.classList.remove('player1');
+    box8.classList.remove('player1');
+    box9.classList.remove('player1');
+    box1.classList.remove('player2');
+    box2.classList.remove('player2');
+    box3.classList.remove('player2');
+    box4.classList.remove('player2');
+    box5.classList.remove('player2');
+    box6.classList.remove('player2');
+    box7.classList.remove('player2');
+    box8.classList.remove('player2');
+    box9.classList.remove('player2');
 });
 
 

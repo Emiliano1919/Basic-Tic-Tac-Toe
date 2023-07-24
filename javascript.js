e46cbf07ac67e6c99e50830abc0eb6fd5fb0306a
Emiliano1919 <emilianojg38@gmail.com>
2023-07-24 09:50:17 -0600

    Add module for game and factory function for players

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
+/*The interactive buttons*/
 let btnStart = document.querySelector("#start");
 let modal = document.querySelector("#modal");
-let closing = document.querySelector(".close-modal");
-let submit = document.querySelector("#submit");
 let main = document.querySelector('.main');
 let btnReStart= document.querySelector('#restart');
 
+/*The entire gameboard*/
 let box1 = document.querySelector(".box1");
 let box2 = document.querySelector(".box2");
 let box3 = document.querySelector(".box3");
@@ -15,6 +15,78 @@ let box7 = document.querySelector(".box7");
 let box8 = document.querySelector(".box8");
 let box9 = document.querySelector(".box9");
 
+/*Global variables*/
+let players=0;
+const winSituations = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+];
+
+/*The objects*/
+const playerFactory=()=>{
+    let score=0;
+    let playerNumber=players++;
+    const addPoint= () => score++;
+    const getScore= () => score;
+    return {addPoint,score};
+};
+
+const gameboard = (() => {
+    let game = [];
+    let result = null;
+    let positions = 9;
+    for(let index = 0; index < 9; index++) {
+        game[index] = ' ';
+        
+    };
+    const player1Play = (n) => {
+        game[n] = 'X';
+        positions--;
+
+    };
+    const player2Play = (n) =>{ 
+        game[n] = 'O';
+        positions--;
+    };
+    
+    const restartGame = () => {
+        for (const key in game) {
+            delete game[key];
+        }
+        positions=9;
+    };
+
+    const whoWon = () => {
+        array.forEach((slash,key) => {
+            if (game[slash[0]]=== 'X' && game[slash[1]] === 'X' && game[slash[2]] === 'X') {
+                result = 'player1';
+                console.log(result);
+                return{result};
+            } else if (game[slash[0]]=== 'O' && game[slash[1]] === 'O' && game[slash[2]] === 'O'){
+                result = 'player2';
+                console.log(result);
+                return{result};
+            } else if(positions===0){
+                result = 'tie';
+                console.log(result);
+                return{result};
+            } else{
+                result=null;
+                console.log(result);
+                return{result};
+            }
+        });
+    };
+    return {player1Play,player2Play,restartGame,whoWon};
+  })();
+
+const player1 = playerFactory();
 
 btnStart.addEventListener('click', () => {
     btnStart.remove();
